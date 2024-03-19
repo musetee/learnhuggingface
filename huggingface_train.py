16d92dbdace43c84e8bca39c01ba55ce51095be0
zy7_oldserver <569915578@163.com>
2024-03-19 13:43:08 +0100

    0319

--- huggingface_train.py
+++ huggingface_train.py
@@ -174,23 +174,24 @@ if __name__ == '__main__':
         in_channels=1,  # the number of input channels, 3 for RGB images
         out_channels=1,  # the number of output channels
         layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+        block_out_channels= (8, 16, 32, 32), #(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
         down_block_types=(
             "DownBlock2D",  # a regular ResNet downsampling block
             "DownBlock2D",
             "DownBlock2D",
             "DownBlock2D",
-            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-            "DownBlock2D",
+            #"AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+            #"DownBlock2D",
         ),
         up_block_types=(
-            "UpBlock2D",  # a regular ResNet upsampling block
-            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+            #"UpBlock2D",  # a regular ResNet upsampling block
+            #"AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
             "UpBlock2D",
             "UpBlock2D",
             "UpBlock2D",
             "UpBlock2D",
         ),
+        norm_num_groups=4,
     )
     
     model = model.to(device)
